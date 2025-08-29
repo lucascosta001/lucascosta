d6724180b8747b68c5a4705eb75d974a7de4d564
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2025-08-29 11:14:06 -0300

    deixie mais inteligente

--- programa001.cpp
+++ programa001.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int x; 
-    x = 4 + 6;
-    cout << "x/5 = " << x/5 << endl;
-    cout << "x*2 = " << x*2 << endl;
+    int x;
+    cout << "Digite um valor para x: ";
+    cin >> x;
+    cout << "x/5 = " << x/5 << "\n";
+    cout << "x*2 = " << x*2 << "\n";
     return 0;
 }
