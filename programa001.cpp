ac5f976df8e9c99f3d89a53ad2ee976dd6cb1bc8
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2025-08-29 11:00:14 -0300

    primeiro programa

--- programa001.cpp
+++ programa001.cpp
@@ -1,1 +1,10 @@
 // uma variavel tem 4 bytes - a não ser se for short, que tem 2 bytesgti 
+#include <iostream>
+using namespace std;
+int main() {
+    int x; 
+    x = 4 + 6;
+    cout << "x/5 = " << x/5 << endl;
+    cout << "x*5 = " << x*5 << endl;
+    return 0;
+}
