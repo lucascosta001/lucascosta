e0bc52713e3c395720aa6a95803871fb3bc4ad84
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-08-31 00:17:51 -0300

    dever de casa, teste 1

---
+++ aula3/homework.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+
+    if(x%2==0)
+        cout << "Par" << "\n";
+    else if(x%2==1)
+        cout << "Impar" << "\n";
+    return 0;
+}
