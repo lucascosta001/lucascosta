6f9726560a40818800538b5da92cc3fbc2251d85
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-08-31 00:33:44 -0300

    adicionei o input

--- aula3/homework.cpp
+++ aula3/homework.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main() {
     int x;
-
+    cout << "Digite um numero: ";
+    cin >> x;
     if(x%2==0)
         cout << "Par" << "\n";
     else if(x%2==1)
         cout << "Impar" << "\n";
-    return 0;
 }
