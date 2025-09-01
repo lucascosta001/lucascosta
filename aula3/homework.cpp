2790d6eb3011e747948791c1f11e78f80a58919d
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-08-31 21:52:08 -0300

    Adiocionei comentarios e especifiquei melhor o codigo

--- aula3/homework.cpp
+++ aula3/homework.cpp
@@ -1,10 +1,12 @@
+// Impar ou par?
 #include <iostream>
-using namespace std;
+using namespace std; 
 int main() {
     int x;
-    cout << "Digite um numero: ";
+    cout << "Digite um numero, real e inteiro: ";
     cin >> x;
-    if(x%2==0)
+    // Todo numero, real e inteiro, que dividido por 2 tem resto 0 é par, e quando o resto e 1 é impar.
+    if(x%2==0) 
         cout << "Voce digitou um numero par" << "\n";
     else if(x%2==1)
         cout << "Voce digitou um numero impar" << "\n";
