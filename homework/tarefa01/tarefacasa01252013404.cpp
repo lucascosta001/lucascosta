17b4a31c27469c218e3fd69686dae351e231b576
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-09-05 16:11:38 -0300

    declarei as variaveis

--- homework/tarefa01/tarefacasa01252013404.cpp
+++ homework/tarefa01/tarefacasa01252013404.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main(){
-   
+/*
+    a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+    00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+
+*/
+    int n1, n2, n3, n4, n5;
+    cout << "Digite um número inteiro: ";
+    cin >> n1;
+    cout << "Digite outro número inteiro: ";
+    cin >> n2;
+    cout << "Digite mais um número inteiro: ";
+    cin >> n3;
+    cout << "Digite o penúltimo número inteiro: ";
+    cin >> n4;
+    cout << "Digite o último número inteiro: ";
+    cin >> n5;
+    cout << n1 << ", " << n2 << ", " << n3 << ", " << n4 << ", " << n5 << "\n";
+    return 0;
 }
