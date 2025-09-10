e316c08002ee14a923c6318e884b3a2811f2552b
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2025-09-10 11:44:07 -0300

    leitor de palavras e frases, com funcionalidades novas

--- aulas/aula5/repeticao.cpp
+++ aulas/aula5/repeticao.cpp
@@ -5,13 +5,15 @@ int main() {
     string palavra;
     cout << "Digite uma palavra: ";
     cin >> palavra;
+    
     int contador = 0, i=0, j=0;   
     i=palavra.length();
     cout << "A palavra digitada foi: " << palavra << "\n";
+
     for (int e = 0; e < i; e++) {
         palavra[e] = tolower(palavra[e]);
     }
-
+    
     while (j < i)
     {
         if (palavra[j] == 'a' || palavra[j] == 'e' || palavra[j] == 'i' || palavra[j] == 'o' || palavra[j] == 'u')
---
+++ aulas/aula5/repeticao2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string frase; // Pode ser alterado para frase
+    cout << "Digite uma frase: ";
+    getline(cin,frase); // Usar getline para ler frases com espaços
+
+    int contador = 0, i=0, j=0;   
+    i=frase.length(); // length da frase
+    cout << "A frase digitada foi: " << frase << "\n";
+
+    for (int e = 0; e < i; e++) { // Converter para minúsculas
+        frase[e] = tolower(frase[e]);
+    }
+
+    while (j < i) // Percorrer a frase
+    {
+        if (frase[j] == 'a' || frase[j] == 'e' || frase[j] == 'i' || frase[j] == 'o' || frase[j] == 'u') // Verificar vogais
+        {
+            cout << "A letra cujo a posicao " << j << " e: ";
+            cout << frase[j] << "; e e uma vogal.\n";
+            contador++;
+        }
+        j++;
+    }
+    cout << "Total de vogais na frase: " << contador << "\n";
+    return 0;
+}
