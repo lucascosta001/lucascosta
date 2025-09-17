4d5673b2807d058a141603738728591a82b17952
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2025-09-17 11:49:35 -0300

    criei um exercicio para testar palindromos

---
+++ aulas/aula7/palindromo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string word;
+    cout << "Digite uma palavra: ";
+    getline(cin, word);
+    string word2;
+    for(int i; word.length > 0; i--) {
+        ne
+    }
+}
