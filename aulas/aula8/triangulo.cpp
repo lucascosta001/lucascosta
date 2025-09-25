0298ab2d05145bdfc25240e56ee69ac46f070278
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-09-25 19:54:52 -0300

    não qual foi a ultima alteração

--- aulas/aula8/triangulo.cpp
+++ aulas/aula8/triangulo.cpp
@@ -4,15 +4,24 @@ using namespace std;
 int main(){
     // numeros triangulares, Eles são formados pela soma de números naturais consecutivos,
     // dada por Tₙ = n(n + 1) / 2
-    cout << "digite um numero inteiro, para saber se e triangular:";
     int num=0;
+    cout << "digite um numero inteiro, para saber se e triangular: ";
     cin >> num;
-    for (int i = 0; i < num; i++)
+    int tria;
+    tria=1;
+    for (int i = 1; i < num; i++)
     {
-        if (true){
+        cout << i << "\n";
+        cout << tria;
+        if (tria==num){
             cout << "funcionou";
-            break;
         }
+        tria = tria+(tria+1);
     }
     return 0;    
 }
+
+// n_posicão e n+(n-1) =
+
+// n = n+(n+1)
+// se n = ao input do usuario entao e um numero triangular, caso contrario nao es
