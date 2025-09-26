0860c1f0a02efde15f7058c19cc1ebe12d0e5f4f
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2025-09-26 10:43:09 -0300

    terminei o exercicio de calcular numeros triangulares!

--- aulas/aula8/triangulo.cpp
+++ aulas/aula8/triangulo.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main(){
     // numeros triangulares, Eles são formados pela soma de números naturais consecutivos,
     // dada por Tₙ = n(n + 1) / 2
-    cout << "digite um numero inteiro, para saber se e triangular:";
-    int num=0;
+    cout << "digite um numero inteiro, para saber se e triangular: ";
+    int num=0, soma;
     cin >> num;
     for (int i = 0; i < num; i++)
     {
-        if (true){
-            cout << "funcionou";
-            break;
-        }
+        soma = soma + i;
+        if (soma == num){
+            cout << num << " e triangular!";
+            return 0;
+        }       
     }
+    cout << num << " nao e triangular!";
     return 0;    
 }
