f2f21fd4da252c377ec70d624430be887c2f89f6
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2025-10-22 11:29:40 -0300

    matrizes com c++

--- aulas/aula12/matriz.cpp
+++ aulas/aula12/matriz.cpp
@@ -2,19 +2,38 @@
 using namespace std;
 
 int main() {
-    int matriz[3][3];
-    for (int linha = 0; linha < 3; linha++) {
-        for (int coluna = 0; coluna < 3; coluna++) {
+    cout << "digite o tamanho da matriz: " << endl;
+    int n = 0;
+    cin >> n;
+    int matriz[n][n];
+    for (int linha = 0; linha < n; linha++) {
+        for (int coluna = 0; coluna < n; coluna++) {
             cout << "Linha " << linha << ", Coluna " << coluna << ": " << endl;
             cin >> matriz[linha][coluna]; // Exemplo de preenchimento
         }
     }
-    for (int linha = 0; linha < 3; linha++) {
-        for (int coluna = 0; coluna < 3; coluna++) {
+    for (int linha = 0; linha < n; linha++) {
+        for (int coluna = 0; coluna < n; coluna++) {
             cout << "[" << linha << "][" << coluna << "]: " << matriz[linha][coluna] << " ";
         }
         cout << endl;
     }
-
+    int soma1 = 0, soma2=0;
+    for (int linha = 0; linha < n; linha++)
+    {
+        for (int coluna = 0; coluna < n; coluna++)
+        {
+            if (linha==coluna)
+            {
+                soma1 += matriz[linha][coluna];
+            }
+            if (linha+coluna==n-1)
+            {
+                soma2 += matriz[linha][coluna];
+            }
+        }
+    }
+    cout << "Soma da Diagonal Principal: " << soma1 << endl;
+    cout << "Soma da Diagonal Secundaria: " << soma2 << endl;
     return 0;
 }
