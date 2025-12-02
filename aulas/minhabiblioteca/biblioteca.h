42f3a363af21655cc8ceddcd163e48c27bec6ab3
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-12-02 13:59:53 -0300

    minha biblioteca para guardar minhas funções

--- aulas/minhabiblioteca/biblioteca.h
+++ aulas/minhabiblioteca/biblioteca.h
@@ -1,6 +1,16 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+#include <iostream>
+#include <vector>
+using namespace std;
 
 float hipotenusa(float cateto1, float cateto2);
 void arvore(int altura);
-// bool megasena1(int, int, int, int, int, int, int, int, int, int);
+bool megasena1(int, int, int, int, int, int, int, int, int, int);
 bool comparaNumero(int n1, int n2);
-void CadastrarProduto(Produto *produtos);
+// void CadastrarProduto(Produto *produtos);
+int fatorial(int);
+unsigned long long fibonacci(int n);
+double euler(int x, int termos);
+
+#endif // BIBLIOTECA_H
--- aulas/minhabiblioteca/minhabiblioteca.cpp
+++ aulas/minhabiblioteca/minhabiblioteca.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-#include "biblioteca.h"
 #include <cmath>
+#include "biblioteca.h"
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
 using namespace std;
 
 float hipotenusa(float cateto1, float cateto2) {
@@ -25,71 +27,109 @@ void arvore(int altura){
     system("pause");
 }
 
-// bool megasena1() {
-//     int acerta = 0;
-//     if (digita1==ale1 || digita1==ale2 || digita1==ale3 || digita1==ale4 || digita1==ale5 || digita1==ale6){
-//         cout << "O numero " << digita1 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     }   
-//     if (digita2==ale1 || digita2==ale2 || digita2==ale3 || digita2==ale4 || digita2==ale5 || digita2==ale6){
-//         cout << "O numero " << digita2 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita3==ale1 || digita3==ale2 || digita3==ale3 || digita3==ale4 || digita3==ale5 || digita3==ale6){
-//         cout << "O numero " << digita3 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita4==ale1 || digita4==ale2 || digita4==ale3 || digita4==ale4 || digita4==ale5 || digita4==ale6){
-//         cout << "O numero " << digita1 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita5==ale1 || digita5==ale2 || digita5==ale3 || digita5==ale4 || digita5==ale5 || digita5==ale6){
-//         cout << "O numero " << digita5 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita6==ale1 || digita6==ale2 || digita6==ale3 || digita6==ale4 || digita6==ale5 || digita6==ale6){
-//         cout << "O numero " << digita6 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita7==ale1 || digita7==ale2 || digita7==ale3 || digita7==ale4 || digita7==ale5 || digita7==ale6){
-//         cout << "O numero " << digita7 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita8==ale1 || digita8==ale2 || digita8==ale3 || digita8==ale4 || digita8==ale5 || digita8==ale6){
-//         cout << "O numero " << digita8 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita9==ale1 || digita9==ale2 || digita9==ale3 || digita9==ale4 || digita9==ale5 || digita9==ale6){
-//         cout << "O numero " << digita9 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     } 
-//     if (digita10==ale1 || digita10==ale2 || digita10==ale3 || digita10==ale4 || digita10==ale5 || digita10==ale6){
-//         cout << "O numero " << digita10 << " esta entre os numeros sorteados.\n";
-//         acerta=acerta+1;
-//     }  
-
+bool megasena1(int digita1, int digita2, int digita3, int digita4, int digita5, int digita6, int digita7, int digita8, int digita9, int digita10) {
+    cout << "Bem vindo ao sorteio da Mega Sena!\n";
+    cout << "Serao sorteados 6 numeros entre 1 e 60.\n";
+    srand(time(0));
+    //Sorteando os 6 numeros
+    int numero1, numero2, numero3, numero4, numero5, numero6;
+    numero1 = rand() % 60 + 1;  
+    numero2 = rand() % 60 + 1;  
+    numero3 = rand() % 60 + 1;  
+    numero4 = rand() % 60 + 1;
+    numero5 = rand() % 60 + 1;
+    numero6 = rand() % 60 + 1;
     
-// }
+    //Verificando numero1
+    int acerta = 0;
+    if (digita1==numero1 || digita1==numero2 || digita1==numero3 || digita1==numero4 || digita1==numero5 || digita1==numero6){
+        cout << "O numero " << digita1 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    }   
+    if (digita2==numero1 || digita2==numero2 || digita2==numero3 || digita2==numero4 || digita2==numero5 || digita2==numero6){
+        cout << "O numero " << digita2 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita3==numero1 || digita3==numero2 || digita3==numero3 || digita3==numero4 || digita3==numero5 || digita3==numero6){
+        cout << "O numero " << digita3 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita4==numero1 || digita4==numero2 || digita4==numero3 || digita4==numero4 || digita4==numero5 || digita4==numero6){
+        cout << "O numero " << digita1 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita5==numero1 || digita5==numero2 || digita5==numero3 || digita5==numero4 || digita5==numero5 || digita5==numero6){
+        cout << "O numero " << digita5 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita6==numero1 || digita6==numero2 || digita6==numero3 || digita6==numero4 || digita6==numero5 || digita6==numero6){
+        cout << "O numero " << digita6 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita7==numero1 || digita7==numero2 || digita7==numero3 || digita7==numero4 || digita7==numero5 || digita7==numero6){
+        cout << "O numero " << digita7 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita8==numero1 || digita8==numero2 || digita8==numero3 || digita8==numero4 || digita8==numero5 || digita8==numero6){
+        cout << "O numero " << digita8 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita9==numero1 || digita9==numero2 || digita9==numero3 || digita9==numero4 || digita9==numero5 || digita9==numero6){
+        cout << "O numero " << digita9 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (digita10==numero1 || digita10==numero2 || digita10==numero3 || digita10==numero4 || digita10==numero5 || digita10==numero6){
+        cout << "O numero " << digita10 << " esta entre os numeros sorteados.\n";
+        acerta=acerta+1;
+    } 
+    if (acerta==4)
+       cout << "Parabens! Voce fez quadra!\n";
+    if (acerta==5) 
+         cout << "Parabens! Voce fez quina!\n";
+    if (acerta < 4) 
+       cout << "Que pena voce somente acertou " << acerta << " numero(s)\n";
+    cout << "Os numeros sorteados foram: " << numero1 << ", " << numero2 << ", " << numero3 << ", " << numero4 << ", " << numero5 << " e " << numero6 << ".\n";
+    if (acerta==6) 
+        return true;
+    else
+        return false;
+}      
+
 
 bool comparaNumero(int n1, int n2) {
     return n1 == n2;
 }
 
-void CadastrarProduto(Produto *p) {
-    cout << "Digite o codigo do produto: \n";
-    cin >> p->codigo;
-    cout << "Digite o nome do produto: \n";
-    getline(cin, p->nome);
+// void CadastrarProduto(Produto *p) {
+//     cout << "Digite o codigo do produto: \n";
+//     cin >> p->codigo;
+//     cout << "Digite o nome do produto: \n";
+//     getline(cin, p->nome);
+// }
+
+// void ListarProdutos(Produto *p, int quant) {
+//     system("cls");
+//     cout << "***** Produtos Cadastrados *****\n";
+//     for(int i=0; i<quant; i++){
+//         cout << "Codigo: " << p[i].codigo << "\n";
+//         cout << "Nome: " << p[i].nome << "\n";
+//         cout << "***************************************************\n";
+//         p++;
+//     }
+//     system("pause");
+// }
+
+int fatorial(int n) {
+    if (n == 0) return 1; // Caso base
+    return n * fatorial(n - 1); // Chamada recursiva
 }
 
-void ListarProdutos(Produto *p, int quant) {
-    system("cls");
-    cout << "***** Produtos Cadastrados *****\n";
-    for(int i=0; i<quant; i++){
-        cout << "Codigo: " << p[i].codigo << "\n";
-        cout << "Nome: " << p[i].nome << "\n";
-        cout << "***************************************************\n";
-        p++;
-    }
-    system("pause");
+unsigned long long fibonacci(int n){
+    if (n <= 1) return n; // Caso base
+    return fibonacci(n - 1) + fibonacci(n - 2); // Chamada recursiva
+}
+
+double euler(int x, int termos) {
+    if (termos <= 0) return 0;
+    return pow(x, termos) / fatorial(termos) + euler(x, termos - 1);
 }
