1e50482151865bcb5297e1cb94e659593de604f4
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-12-02 13:54:13 -0300

    aula de funções

--- aulas/aulaFuncao/megasena.cpp
+++ aulas/aulaFuncao/megasena.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include "biblioteca.h"
+#include "C:\Users\lucas\Documentos\estudos\unb\c++\lucascosta\aulas\minhabiblioteca\biblioteca.h"
 using namespace std;
 
 int main() {
@@ -52,15 +52,16 @@ int main() {
     cout << "Digite seu 10 numero: ";
     cin >> digita10;
     //Verificando ale1
-    if (megasena(digita1, digita2, digita3, digita4, digita5, digita6, digita7, digita8, digita9, digita10)) {
+    if (megasena1(digita1, digita2, digita3, digita4, digita5, digita6, digita7, digita8, digita9, digita10)) {
         cout << "Voce ganhou na Mega Sena!\n";
         cout << "Parabens! Voce fez quadra!\n";
-    if (acerta==5) 
-        cout << "Parabens! Voce fez quina!\n";
-    if (acerta==6)
-        cout <<  "Parabens! Voce fez sena!\n";
-    if (acerta < 4) 
-       cout << "Que pena voce somente acertou " << acerta << " numero(s)\n";
+    }
+    // if (acerta==5) 
+    //     cout << "Parabens! Voce fez quina!\n";
+    // if (acerta==6)
+    //     cout <<  "Parabens! Voce fez sena!\n";
+    // if (acerta < 4) 
+    //    cout << "Que pena voce somente acertou " << acerta << " numero(s)\n";
     cout << "Os numeros sorteados foram: " << ale1 << ", " << ale2 << ", " << ale3 << ", " << ale4 << ", " << ale5 << " e " << ale6 << ".\n";
     return 0;
 }
